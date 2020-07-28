8ab4e26ff1e8646a4c08f6792f6b11c7309c4304
Nicolas Verger <nicolas.india@gmail.com>
2020-07-28 15:00:53 +0200

    uibuild

--- index.js
+++ index.js
@@ -6,70 +6,61 @@ const Note = require('./models/note')
 const cors = require('cors')
 const morgan = require('morgan')
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
-]
-
-app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
-app.use(express.static('build'))
 
 app.get('/', (req, res) => {
     res.send('<h1>Notes application</h1>')
 })
 
-app.get('/api/notes', async (req, res) => {
+app.get('/api/notes', async (req, res, next) => {
     try {
         const notes = await Note.find({})
-        res.json(notes)
+
+        if (notes) {
+            res.json(notes)
+        } else {
+            res.status(404).end()
+        }
     } catch (error) {
-        res.status(404).end()
+        next(error)
     }
 })
 
-app.get('/api/notes/:id', async (req, res) => {
+app.get('/api/notes/:id', async (req, res, next) => {
     try {
         const note = await Note.findById(req.params.id)
-        res.json(note)
+
+        if (note) {
+            res.json(note)
+        } else {
+            res.status(404).end()
+        }
     } catch (error) {
-        res.status(404).end()
+        next(error)
     }
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-    
-    res.status(204).end()
+app.delete('/api/notes/:id', async (req, res, next) => {
+    try {
+        await Note.findByIdAndRemove(req.params.id)
+        res.status(204).end()
+    } catch (error) {
+        next(error)
+    }
 })
 
-app.post('/api/notes', async (req, res) => {
+app.post('/api/notes', async (req, res, next) => {
     const body = req.body
 
-    if (!body.content) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+    // if (!body.content) {
+    //     return res.status(400).json({
+    //         error: 'content missing'
+    //     })
+    // }
 
     const note = new Note({
         content: body.content,
@@ -81,7 +72,23 @@ app.post('/api/notes', async (req, res) => {
         const savedNote = await note.save() 
         res.json(savedNote) 
     } catch (error) {
-        console.log(error)
+        next(error)
+    }
+})
+
+app.put('/api/notes/:id', async (req, res, next) => {
+    const body = req.body
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    try {
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        res.json(updatedNote)
+    } catch (error) {
+        next(error)
     }
 })
 
@@ -90,6 +97,22 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError' || error.name === 'ReferenceError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    if (error.name === 'ValidationError') {
+        return res.status(400).send({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
--- models/note.js
+++ models/note.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+mongoose.set('useFindAndModify', false)
+
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
@@ -13,8 +15,15 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minlength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean
 })
 
